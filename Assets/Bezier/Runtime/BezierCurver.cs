825db12be56e0628f69f15372ff1994cf76fabf1
Francisco Lucas <franciscocostaalves2@gmail.com>
2019-12-06 16:03:58 -0300

    Optimized bezier curver

--- Assets/Bezier/Runtime/BezierCurver.cs
+++ Assets/Bezier/Runtime/BezierCurver.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using static Bezier.BezierUtility;
 
 namespace Bezier
 {
@@ -6,6 +7,12 @@ namespace Bezier
   {
     [HideInInspector]
     public Point[] points;
+    public int Lenght => points.Length;
+
+    // Cache
+    private bool isCacheWorldPoints;
+    private Point[] worldPoints;
+    private Transform cacheTransform;
 
     public BezierCurver()
     {
@@ -16,6 +23,52 @@ namespace Bezier
       var point2 = new Point(Vector3.forward * 2, tangent1, tangent2);
 
       points = new Point[] { point1, point2 };
+    }
+
+    public Point GetWorldPoint(int index)
+    {
+      return (isCacheWorldPoints) ? worldPoints[index] : LocalToWorldPoint(points[index], GetTransform());
+    }
+
+    public void SetWorldPoint(int index, Point worldPoint)
+    {
+      var point = WorldToLocalPoint(worldPoint, GetTransform());
+      points[index] = point;
+
+      if(isCacheWorldPoints)
+      {
+        worldPoints[index] = worldPoint;
+      }
+    }
+
+    public Point[] GetWorldPoints()
+    {
+      if (isCacheWorldPoints)
+      {
+        return worldPoints;
+      }
+
+      worldPoints = LocalToWorldPoints(points, GetTransform());
+      isCacheWorldPoints = true;
+      return worldPoints;
+    }
+
+    public void SetWorldPoints(Point[] worldPoints)
+    {
+      this.worldPoints = worldPoints;
+      isCacheWorldPoints = true;
+
+      points = WorldToLocalPoints(worldPoints, GetTransform());
+    }
+
+    public Transform GetTransform()
+    {
+      if (cacheTransform == null)
+      {
+        cacheTransform = transform;
+      }
+
+      return cacheTransform;
     }
   }
 }
