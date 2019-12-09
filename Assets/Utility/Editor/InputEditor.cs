d2e128be10a13bd1b2c52016acf23814a6833b29
Francisco Lucas <franciscocostaalves2@gmail.com>
2019-12-09 16:43:48 -0300

    Refactoring BezierCurverEditor.cs

---
+++ Assets/Utility/Editor/InputEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Input
+{
+  public static class InputEditor
+  {
+    public static bool GetKeyDown(KeyCode key, out Event currentEvent)
+    {
+      currentEvent = Event.current;
+      return currentEvent.type == EventType.KeyDown && currentEvent.keyCode == key;
+    }
+
+    public static bool GetMouseDown(int mouse, out Event currentEvent)
+    {
+      currentEvent = Event.current;
+      return currentEvent.type == EventType.MouseDown && currentEvent.button == mouse;
+    }
+  }
+}
